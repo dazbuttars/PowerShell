cf73ad53aaff4ae88c19e9f46e7ccc1b7511547c
Daz Buttars <dazbuttars@Dazs-MacBook-Pro.local>
2020-07-20 12:09:55 -0600

    Added office install

--- Scripts/WindowsPrep.ps1
+++ Scripts/WindowsPrep.ps1
@@ -2,7 +2,7 @@
 #Unpin Taskbar Store and Mail
 function UnPinFromTaskbar { param( [string]$appname )
 Try {
-((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object{$_.Name -eq $appname}).Verbs() | ?{$_.Name -like "Ta bort fr*" -OR $_.Name -like "Unpin from*"} | ForEach-Object{$_.DoIt()}
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object{$_.Name -eq $appname}).Verbs() | Where-Object{$_.Name -like "Ta bort fr*" -OR $_.Name -like "Unpin from*"} | ForEach-Object{$_.DoIt()}
 } Catch {$a="b"}
 } #UnPinFromTaskbar
 UnPinFromTaskbar "E-Post"
@@ -14,7 +14,10 @@ UnPinFromTaskbar "Mail"
 $url = "https://ninite.com/7zip-air-chrome-firefox-silverlight-spybot2-vlc/ninite.exe"
 $outpath = ".\ninite.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
+
+#Check to see if OS is 64bit
 $version = [System.Environment]::Is64BitOperatingSystem 
+
 if($version -eq $true){
 #Download Java Windows 64bit
 $url = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242060_3d5a2bb8f8d4428bbe94aed7ec7ae784"
@@ -35,7 +38,7 @@ Start-Process -FilePath ".\ninite.exe" -Wait
 
 #Do you want to install Reader?
 do{
-$i = 0
+$r = 0
 $UsersAnswer = Read-Host -Prompt 'Do you want to install Adobe Reader? (Yes/No)'
 if ($UsersAnswer -eq 'Yes'){
 
@@ -46,13 +49,37 @@ Invoke-WebRequest -Uri $url -OutFile $outpath
 
 #Install
 Start-Process -FilePath ".\readerdc_en_xa_crd_install.exe" -ArgumentList '/passive' -Wait
-$i = 1
+$r = 1
 }
 elseif ($UsersAnswer -eq 'No'){
     Write-Host 'O.K.'
-    $i = 1
+    $r = 1
  }
  else{
     Write-Host 'Type Yes or No.'
  }
- } while ($i -eq 0)
+ } while ($r -eq 0)
+
+ #Do you want to install Libreoffice?
+do{
+   $l = 0
+   $UsersAnswer = Read-Host -Prompt 'Do you want to install Libreoffice? (Yes/No)'
+   if ($UsersAnswer -eq 'Yes'){
+   
+   #Download Libreoffice
+   $url = "https://ninite.com/libreoffice/ninite.exe"
+   $outpath = ".\office.exe"
+   Invoke-WebRequest -Uri $url -OutFile $outpath
+   
+   #Install
+   Start-Process -FilePath ".\office.exe" -Wait
+   $l = 1
+   }
+   elseif ($UsersAnswer -eq 'No'){
+       Write-Host 'O.K.'
+       $l = 1
+    }
+    else{
+       Write-Host 'Type Yes or No.'
+    }
+    } while ($l -eq 0)
