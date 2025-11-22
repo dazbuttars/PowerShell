77037294d64050992ca59bdb6c51788b20139587
dazbuttars <dbuttars12@gmail.com>
2025-11-21 17:00:34 -0700

    Add files via upload
    Script for deleting files older than the date chosen.

---
+++ Scripts/ScheduledFileCleanUp.ps1
@@ -0,0 +1,4 @@
+﻿#Deletes file types older than specified date in months from the C:\the\designated\spot directory every scheduled date via a scheduled task
+$targetDate = (Get-Date).AddMonths(-2)
+$tagetFiles = Get-ChildItem -Path "C:\the\designated\spot" -File -Filter "*.fileType" | Where-Object { $_.LastWriteTime -lt $targetDate } 
+$tagetFiles | Remove-Item
